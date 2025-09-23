e8d9a2855508c75a9d381609879a67e8604b06ae
Jacob Dury <jacobdury2007@gmail.com>
2025-09-23 15:12:27 -0400

    ajout fonction 1-5

--- src/packagetp1/Employe.java
+++ src/packagetp1/Employe.java
@@ -11,6 +11,9 @@ public class Employe {
     // constante(s): static final UPPER_SNAKE_CASE
     // EX: public static final int UNE_CONSTANTE = 3;
 
+    public static final double IMPOT_FEDERAL = 0.19;
+    public static final double IMPOT_PROVINCIAL = 0.2;
+
     // méthode constructeur
     public Employe(String nom, String numero, double salaireHoraire, double nbreHeuresSemaine, int anciennete) {
         this.nom = nom;
@@ -18,5 +21,26 @@ public class Employe {
         this.salaireHoraire = salaireHoraire;
         this.nbreHeuresSemaine = nbreHeuresSemaine;
         this.anciennete = anciennete;
+    }
+
+    public double salaireBrut(){
+        return this.nbreHeuresSemaine * this.salaireHoraire;
+    }
+    public double salaireNetAvantImpot(){
+        double salaire = salaireBrut();
+        salaire -= salaireBrut() * 0.111;
+        salaire -= salaireBrut() * 0.0136;
+        salaire -= 20;
+        return salaire < 0 ? 0 : salaire;
+
+    }
+    public double impotFederal(){
+        return salaireNetAvantImpot() * IMPOT_FEDERAL;
+    }
+    public double impotProvincial(){
+        return salaireNetAvantImpot() * IMPOT_PROVINCIAL;
+    }
+    public double salaireNetApresImpot(){
+        return (salaireNetAvantImpot() - impotFederal()) - impotProvincial();
     }
 }
